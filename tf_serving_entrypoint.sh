f962780a7a5f35fcb2f48bf038f300bb1c34505c
richardlotw <107301714+richardlotw@users.noreply.github.com>
2024-04-18 23:37:32 +0800

    Update tf_serving_entrypoint.sh

--- tf_serving_entrypoint.sh
+++ tf_serving_entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-tensorflow_model_server --port=8500 --rest_api_port="${PORT}" --model_config_file="${MODEL_CONFIG_FILE}"  "$@"
+tensorflow_model_server --port=8500 --rest_api_port="${PORT}" --model_name=model --model_base_path="/"  "$@"
 
 
